3066fb67b6bc5f40c2feded0bd3e5fb5fcff3d3e
manuel <bravopaezm.15@gmail.com>
2024-11-03 16:19:58 +0100

    Add model Image

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -3,15 +3,21 @@ from django.conf import settings
 from django.utils.text import slugify
 
 
-class Image(models.Models):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='images_created',
-                             on_delete=models.CASCADE)
+class Image(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='images_created',
+        on_delete=models.CASCADE,
+    )
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, blank=True)
     url = models.URLField(max_length=2000)
     image = models.ImageField(upload_to='images/%Y/%m/%d/')
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True)
+    users_like = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name='images_liked', blank=True
+    )
 
     class Meta:
         indexes = [
@@ -26,4 +32,3 @@ class Image(models.Models):
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
-
