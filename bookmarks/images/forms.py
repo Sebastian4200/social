935e7d415462abb2e2ffb662a1ba6a9ab8d4b0c7
Manuel <bravopaezm.15@gmail.com>
2024-11-03 19:11:54 +0100

    Add forms and views in Images

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -1,11 +1,14 @@
+import requests
 from django import forms
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
 from .models import Image
 
 
 class ImageCreateForm(forms.ModelForm):
     class Meta:
         model = Image
-        field = ['title', 'url', 'description']
+        fields = ['title', 'url', 'description']
         widgets = {
             'url': forms.HiddenInput,
         }
@@ -21,3 +24,19 @@ class ImageCreateForm(forms.ModelForm):
             )
 
         return url
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(self.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+        # download image from the given URL
+        response = requests.get(image_url)
+        image.image.save(image_name, ContentFile(response.content), save=False)
+
+        if commit:
+            image.save()
+
+        return image
+
